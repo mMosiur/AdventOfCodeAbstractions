23d83d67005542ce3941fd7f1f45282f94cdcccd
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-05-29 16:20:31 +0200

    Added docstring to DaySolver

--- DaySolver.cs
+++ DaySolver.cs
@@ -4,10 +4,21 @@ using System.IO;
 
 namespace AdventOfCode.Abstractions;
 
+/// <summary>
+/// Abstract class that represents a generic solver for a day of Advent of Code.
+/// It is a base class for all solvers and provides a common interface of methods for them,
+/// as well as a common implementation for input file reading.
+/// </summary>
 public abstract class DaySolver
 {
+	/// <summary>
+	/// The content that was read from the input file.
+	/// </summary>
 	public string Input { get; }
 
+	/// <summary>
+	/// The lines of the input file (excluding one last empty line after newline break).
+	/// </summary>
 	public IEnumerable<string> InputLines
 	{
 		get
@@ -21,13 +32,24 @@ public abstract class DaySolver
 		}
 	}
 
+	/// <summary>
+	/// A constructor that reads the input file from a location provided by <paramref name="options"/> and stores its content.
+	/// </summary>
 	public DaySolver(DaySolverOptions options)
 	{
 		ArgumentNullException.ThrowIfNull(options.InputFilePath);
 		Input = File.ReadAllText(options.InputFilePath);
 	}
 
+	/// <summary>
+	/// A method that solves the first part of the day puzzle.
+	/// </summary>
+	/// <returns>The solution of the first part of the day puzzle.</returns>
 	public abstract string SolvePart1();
 
+	/// <summary>
+	/// A method that solves the second part of the day puzzle.
+	/// </summary>
+	/// <returns>The solution of the second part of the day puzzle.</returns>
 	public abstract string SolvePart2();
 }
