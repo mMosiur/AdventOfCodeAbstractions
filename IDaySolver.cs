49135d901dce02273050adec30b988ab7a5e04b5
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-10-28 22:58:36 +0200

    Remove unnecessary public modifier in interface properties

--- IDaySolver.cs
+++ IDaySolver.cs
@@ -5,17 +5,17 @@ public interface IDaySolver
 	/// <summary>
 	/// Property <c>Year</c> represents the year of the Advent of Code event the puzzle of the <see cref="DaySolver"/> belongs to.
 	/// </summary>
-	public int Year { get; }
+	int Year { get; }
 
 	/// <summary>
 	/// Property <c>Day</c> represents the day of the Advent of Code event the puzzle of the <see cref="DaySolver"/> belongs to.
 	/// </summary>
-	public int Day { get; }
+	int Day { get; }
 
 	/// <summary>
 	/// Property <c>Title</c> represents the title of the day of the Advent of Code event the <see cref="DaySolver"/> belongs to.
 	/// </summary>
-	public string Title { get; }
+	string Title { get; }
 
 	/// <summary>
 	/// A method that solves the first part of the day puzzle.
