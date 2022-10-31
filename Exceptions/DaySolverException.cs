5bae282238b78fa143f35f163f7ef6f8b16c5ada
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-10-31 23:21:39 +0100

    Added extended top-level exception (#5)

---
+++ Exceptions/DaySolverException.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace AdventOfCode;
+
+/// <summary>
+/// Exception thrown when a day solver fails to solve a puzzle.
+/// </summary>
+[Serializable]
+public class DaySolverException : Exception
+{
+	/// <inheritdoc />
+	public DaySolverException() { }
+
+	/// <inheritdoc />
+	public DaySolverException(string message) : base(message) { }
+
+	/// <inheritdoc />
+	public DaySolverException(string message, Exception innerException) : base(message, innerException) { }
+
+	/// <inheritdoc />
+	protected DaySolverException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+}
---
+++ Exceptions/InputException.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace AdventOfCode;
+
+/// <summary>
+/// An exception thrown when the puzzle input could not be properly interpreted.
+/// </summary>
+[Serializable]
+public class InputException : Exception
+{
+	/// <inheritdoc />
+	public InputException() { }
+
+	/// <inheritdoc />
+	public InputException(string message) : base(message) { }
+
+	/// <inheritdoc />
+	public InputException(string message, Exception innerException) : base(message, innerException) { }
+
+	/// <inheritdoc />
+	protected InputException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+}
