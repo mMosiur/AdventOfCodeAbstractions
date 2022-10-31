05d00eaf28a617ad88460e282a3399518607ace2
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-10-31 23:15:17 +0100

    Added extended top-level exception

---
+++ Exceptions/CommandLineException.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace AdventOfCode;
+
+/// <summary>
+/// Exception thrown when the command line arguments are invalid.
+/// </summary>
+[Serializable]
+public class CommandLineException : Exception
+{
+	/// <inheritdoc />
+	public CommandLineException() { }
+
+	/// <inheritdoc />
+	public CommandLineException(string message) : base(message) { }
+
+	/// <inheritdoc />
+	public CommandLineException(string message, Exception innerException) : base(message, innerException) { }
+
+	/// <inheritdoc />
+	protected CommandLineException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+}
